d88d1d8fe5472929a1a98515f5e0ea6adb298991
sunyb3 <a52856458@163.com>
2017-01-04 13:12:26 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/easyweather/sunyb3/easyweather/db/County.java
@@ -0,0 +1,46 @@
+package com.easyweather.sunyb3.easyweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by jyh on 2017/01/04.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
